f209481e7dbadd0012c5e95b9b88d9a761464b19
Maverick Wong <mvrck.wong@gmail.com>
2025-10-14 23:03:25 +0800

    Developed further US data

--- dbt/models/01-silver/silver_covid_cases_us.sql
+++ dbt/models/01-silver/silver_covid_cases_us.sql
@@ -1,7 +1,19 @@
 {{
     config(
-        enabled=false
+        materialized='table'
     )
 }}
 
-SELECT 1
+-- Dynamically union all bronze tables that start with "us_"
+{% set relations = dbt_utils.get_relations_by_pattern(
+    schema_pattern='bronze',
+    table_pattern='us_%'
+) %}
+
+with unioned_data as (
+    {{ dbt_utils.union_relations(
+        relations=relations,
+        source_column_name='_source'
+    ) }}
+)
+SELECT * FROM unioned_data
